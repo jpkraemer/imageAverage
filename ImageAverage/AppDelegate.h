8e739fd0a3b47b2db80e8acf5d04e0a971037172
Jan-Peter Kraemer <jkramer@adobe.com>
2014-08-21 13:56:10 -0700

    Initial Commit

---
+++ ImageAverage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImageAverage
+//
+//  Created by jkramer on 8/21/14.
+//  Copyright (c) 2014 Adobe Systems Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ImageAverage/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ImageAverage
+//
+//  Created by jkramer on 8/21/14.
+//  Copyright (c) 2014 Adobe Systems Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
